52ff5435b4c19c7d4e30c59ce577128a69c6d759
lucasfl <lucasssfelix@gmail.com>
2015-10-01 23:38:03 -0300

    backend alterado para servidor

--- trabalho1/backend.py
+++ trabalho1/backend.py
@@ -1,12 +1,17 @@
 import socket
 PORT = 10000
-HOST = '192.168.0.2'
+HOST = ''
 cSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-cSocket.connect((HOST,PORT))
+#cSocket.connect((HOST,PORT))
+cSocket.bind((HOST,PORT))
+cSocket.listen(10)
+(sSocket, address) = cSocket.accept()
+print "msg TESTE"
 msg = raw_input()
 while msg <> '\x18':
-	cSocket.send(msg)
-	resposta = cSocket.recv(4096)
+	sSocket.send(msg)
+	resposta = sSocket.recv(4096)
 	print str(resposta)
 	msg = raw_input()
 cSocket.close()
+
