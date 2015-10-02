7a69a126f5af798c8190672ba93e8cf81ec5f7db
fuedy <andrecpg@gmail.com>
2015-10-01 23:59:59 -0300

    Revert "backend alterado para servidor"
    This reverts commit 52ff5435b4c19c7d4e30c59ce577128a69c6d759.

--- trabalho1/backend.py
+++ trabalho1/backend.py
@@ -1,17 +1,12 @@
 import socket
 PORT = 10000
-HOST = ''
+HOST = '192.168.0.2'
 cSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#cSocket.connect((HOST,PORT))
-cSocket.bind((HOST,PORT))
-cSocket.listen(10)
-(sSocket, address) = cSocket.accept()
-print "msg TESTE"
+cSocket.connect((HOST,PORT))
 msg = raw_input()
 while msg <> '\x18':
-	sSocket.send(msg)
-	resposta = sSocket.recv(4096)
+	cSocket.send(msg)
+	resposta = cSocket.recv(4096)
 	print str(resposta)
 	msg = raw_input()
 cSocket.close()
-
