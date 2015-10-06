8623670e5a6f5ae314e7ffbefb8f8a14062f0515
¨rteixeiragarcia¨ <rteixeiragarcia@gmail.com>
2015-10-06 20:25:45 -0300

    Backend acoplado ao webserver e index novo

--- trabalho1/webserver.py
+++ trabalho1/webserver.py
@@ -1,22 +1,40 @@
+#!/usr/bin/python
+
 import socket
 import cgi, cgitb
 
 PORT = 10000
 HOST = {1:"192.168.0.5", 2:"192.168.0.16"}
 COMANDOS = {1:"ps", 2:"df", 3:"finger", 4:"uptime"}
-respostas = []
-lista_comandos = []
+maior = ">"
+ponto = ";"
+pipe = "|"
 
 form = cgi.FieldStorage()
 
+print "Content-type:text/html"
+print
+print "<html>"
+print "<head>"
+print "<title>Trabalho de Redes 1</title>"
+print "</head>"
+print "<body>"
+
 for i in range(1,3):
 	cSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	print "<h2>" + "MAQUINA " + str(i) + "</h2>"
 	cSocket.connect((HOST[i],PORT))
-	for j in range(1,4):
+	for j in range(1,5):
 		if (form.getvalue("maq"+str(i)+"_"+COMANDOS[j])):
 			parametro = form.getvalue("maq"+str(i)+"-"+COMANDOS[j])
-			cSocket.send("REQUEST "+str(j)+" "+parametro)
-			resposta = cSocket.recv(4096)
-			lista_comandos.append(COMANDOS[j])
-			respostas.append(resposta)
+			if (str(parametro) == "None" or maior in str(parametro) or ponto in str(parametro) or pipe in str(parametro)):
+				parametro = ""
+			print "<h4>" + str(COMANDOS[j]) + " " + str(parametro) + "</h4>"
+			cSocket.send("REQUEST "+str(j)+" "+str(parametro))
+			resposta = cSocket.recv(16384)
+			resposta = resposta.replace("\n", "<br>")
+			print "<p>" + str(resposta[11:]) + "</p>"
 	cSocket.close()
+
+print "</body>"
+print "</html>"
