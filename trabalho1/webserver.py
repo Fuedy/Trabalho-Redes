c95803ac897fece9d753b304d17855f68c052a09
¨rteixeiragarcia¨ <rteixeiragarcia@gmail.com>
2015-10-06 21:05:27 -0300

    Webserver comentado

--- trabalho1/webserver.py
+++ trabalho1/webserver.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python
+#necessario colocar caminho do python para uso do apache
 
+#Importada bibliotecas para funcionamento dos sockets e scripts cgi
 import socket
 import cgi, cgitb
 
+#Porta de comunicacao previamente definida
 PORT = 10000
+
+#HOST e um vetor com os ip's das maquinas onde os daemons estao presentes
 HOST = {1:"192.168.0.5", 2:"192.168.0.16"}
+
+#COMANDOS e um vetor com os comandos que podem ser requisitados as maquinas
 COMANDOS = {1:"ps", 2:"df", 3:"finger", 4:"uptime"}
+
+#definicao dos caracteres invalidos para consulta as maquinas
 maior = ">"
 ponto = ";"
 pipe = "|"
 
+#o formulario submetido pelo servidor e lido e guardado
 form = cgi.FieldStorage()
 
+#comeco da criacao da pagina de resposta com as respostas dos comandos realizados nas maquinas
 print "Content-type:text/html"
 print
 print "<html>"
@@ -20,21 +31,50 @@ print "<title>Trabalho de Redes 1</title>"
 print "</head>"
 print "<body>"
 
-for i in range(1,3):
+#loop externo responsavel por fazer controle de todas as tres maquinas possiveis de comunicacao
+#foi definido que e possivel comunicar com ate tres maquinas e requisitar comandos
+for i in range(1,4):
+
+	#criacao de um socket que se comunica com protocolo v4 de enderecos e cria um servico confiavel de entrega de pacote
 	cSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+	#e exibido na pagina o nome e numero da maquina em questao 
 	print "<h2>" + "MAQUINA " + str(i) + "</h2>"
+
+	#o socket criado acima e conectado com uma das maquinas utilizando a porta definida acima e o primeiro dos enderecos tambem definido acima
 	cSocket.connect((HOST[i],PORT))
+
+	#loop interno para controle dos comandos possiveis de ser requisitados para cada maquina
 	for j in range(1,5):
+
+		#condicao para verificar se a caixa de marcacao do comando, no formulario, esta marcada
 		if (form.getvalue("maq"+str(i)+"_"+COMANDOS[j])):
+
+			#caso a caixa esteja marcada, e armazenado em uma variavel o parametro selecionado
 			parametro = form.getvalue("maq"+str(i)+"-"+COMANDOS[j])
+
+			#condicao para exclusao de parametros invalidos para requisicao
 			if (str(parametro) == "None" or maior in str(parametro) or ponto in str(parametro) or pipe in str(parametro)):
 				parametro = ""
+
+			#exibicao do nome do comando junto com o parametro requisitado, caso ele exista
 			print "<h4>" + str(COMANDOS[j]) + " " + str(parametro) + "</h4>"
+
+			#envio do request, atraves do socket, para o daemon localizado na maquina em questao
 			cSocket.send("REQUEST "+str(j)+" "+str(parametro))
+
+			#resposta recebida da maquina. Armazenada em uma variavel
 			resposta = cSocket.recv(16384)
+
+			#substituicao da quebra de linha da resposta recebida para o formato em html. Feito para melhor visualizacao na pagina html de resposta dos comandos
 			resposta = resposta.replace("\n", "<br>")
+
+			#exibicao da resposta do comando requisitado
 			print "<p>" + str(resposta[11:]) + "</p>"
+
+	#fechamento do socket aberto para comunicao com a maquina
 	cSocket.close()
 
+#fim da pagina html de resposta
 print "</body>"
 print "</html>"
