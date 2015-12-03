e2d68d94f365527c2654c27d2db28bd8e90a3b6f
¨rteixeiragarcia¨ <rteixeiragarcia@gmail.com>
2015-12-03 17:42:11 -0200

    envio de arquivo

--- trabalho2/client/client.py
+++ trabalho2/client/client.py
@@ -2,10 +2,52 @@ from socket import *
 import sys
 import select
 
+def AppendChunk(sSourceFilespec, fDestinationFileHandle):
+
+    fInput = open(sSourceFilespec, "rb") 
+    
+    while True:
+        byte = fInput.read(1) 
+
+        if byte: 
+            fDestinationFileHandle.write(byte)
+        else:
+            fInput.close()
+            break
+
+
+    
+def RebuildChunkedFile(sSourcePath, sFilename, sDestinationPath, sChunkPrefix):
+    
+    import os.path    
+    sDestinationFilespec = sDestinationPath +  sFilename
+    
+    if os.path.isfile(sDestinationFilespec) :
+
+        print "Destination already exists: " + sDestinationFilespec
+        return None
+    
+    fDestinationFile = open(sDestinationFilespec, "wb")
+    
+    nChunkNumber = 1
+    while True:
+
+        sSourceFilespec = sSourcePath + sChunkPrefix + str(nChunkNumber) + "_" + sFilename
+
+        if os.path.isfile(sSourceFilespec):
+            AppendChunk(sSourceFilespec, fDestinationFile)
+            os.remove(sSourceFilespec)
+            print "reading " + sSourceFilespec
+            nChunkNumber = nChunkNumber + 1
+        else:
+            break
+            
+            
+    fDestinationFile.close()
+
 host=sys.argv[1]
 port = int(sys.argv[2])
 s = socket(AF_INET,SOCK_DGRAM)
-#s.bind((host,port))
 nomeArquivo = sys.argv[3]
 
 addr = (host,port)
@@ -16,15 +58,21 @@ s.sendto(nomeArquivo,addr)
 data,addr = s.recvfrom(buf)
 
 print "Received File:",data.strip()
-f = open(data.strip(),'wb')
+numerochunk = 1
 
-data,addr = s.recvfrom(buf)
-try:
-    while(data):
-        f.write(data)
-        s.settimeout(2)
-        data,addr = s.recvfrom(buf)
-except timeout:
-    f.close()
-    s.close()
-    print "File Downloaded"
+while True:
+	try:
+		pedaco = "chunk" + str(numerochunk) + "_" + nomeArquivo
+		f = open(pedaco,'wb')
+		data,addr = s.recvfrom(buf)
+		s.sendto("ack", addr)
+		f.write(data)
+		s.settimeout(1)
+		numerochunk = numerochunk + 1
+	except timeout:
+		f.close()
+		s.close()
+		print "File Downloaded"
+		break
+
+RebuildChunkedFile("./", str(nomeArquivo), "./", "chunk")
--- trabalho2/server/server.py
+++ trabalho2/server/server.py
@@ -1,24 +1,61 @@
 from socket import *
 import sys
+import os.path
+
+def WriteChunk(fInput, fOutput, nMaxBytes):
+    
+    for i in xrange(nMaxBytes - 1): # loop until nMaxBytes or out of data
+
+        byte = fInput.read(1) # read a byte
+        
+        if byte: # if we got one, write it, otherwise, close fInput and leave
+            fOutput.write(byte)
+        else:
+            fInput.close()
+            
+            break
+
+    fOutput.close()
+
+def WriteChunkFiles(sInputFilepath, sInputFilename, sOutputFilepath, sPrefix, nChunkSize):
+
+	sInputFilespec = sInputFilepath + sInputFilename
+	nChunkNumber = 1
+
+	with open(sInputFilespec, "rb") as f:
+	    while True:
+
+	        sOutputFilespec = sOutputFilepath + sPrefix + str(nChunkNumber) + "_" + sInputFilename
+	        out = open(sOutputFilespec, "wb")
+
+	        WriteChunk(f, out, nChunkSize)
+
+	        g = open(sOutputFilespec, "rb")
+	        data = g.read(buf)
+	        s.sendto(data,addr)
+	        mensagem = s.recvfrom(buf)
+         	print str(mensagem)
+         	os.remove(sOutputFilespec)
+
+	        if f.closed:
+	            break
+	            
+	        nChunkNumber = nChunkNumber + 1
+
+
 
 s = socket(AF_INET,SOCK_DGRAM)
-host ="127.0.0.1"
+host = "127.0.0.1"
 port = int(sys.argv[1])
-buf =1024
+buf = 1024
 s.bind((host,port))
 addr = (host,port)
-
 
 file_name,addr = s.recvfrom(buf)
 print file_name
 
 s.sendto(file_name,addr)
 
-f=open(file_name,"rb")
-data = f.read(buf)
-while (data):
-    if(s.sendto(data,addr)):
-        print "sending ..."
-        data = f.read(buf)
+WriteChunkFiles("./", str(file_name), "./", "chunk", 1024)
+
 s.close()
-f.close()
